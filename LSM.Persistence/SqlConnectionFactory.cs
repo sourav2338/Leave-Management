dda73aa90bec5178742422759590de8f9a4e796a
Sourav2338 <sourav.cbr2016@gmail.com>
2026-09-19 09:22:33 +0530

    first commit

---
+++ LSM.Persistence/SqlConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace LSM.Persistence
+{
+    public class SqlConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString=configuration.GetConnectionString("DefaultConnection")?? throw new InvalidOperationException("DefaultConnection string is not configured.");
+        }
+        public IDbConnection CreateConnection()=> new SqlConnection(_connectionString);
+
+    }
+}
